cccc0f8dcc218593bf617aeda8c9bfde3e6f46cf
Damon-legend <tychicusbrahe@gmail.com>
2026-02-22 22:28:15 +0100

    Add files via upload

---
+++ password.py
@@ -0,0 +1,13 @@
+#Write a program that asks the user to enter a password. If the user enters the right password,the program should tell them they are logged in to the system. otherwise, the programshould ask them to re-enter the password. The user should only get five tries to enter thepassword, after which point the program should tell them that they are kicked off of thesystem.
+
+password = int(input("Enter a password: "))
+user_password = sambdf5
+
+while password != "user_password" :
+    password = int(input("Re-enter a password: "))
+if input(password) > 4 :
+    print("you are kicked out of the system")
+else :
+    print("you are logged in !!!")
+    
+
